d1489ae363ebb3d28a835aef076621c25265df25
Kayla <k.kotewall@gmail.com>
2018-03-04 15:20:09 -0600

    orm start

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,4 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
+// allow handlebars to see everything in db table so that it can display all the items on the html; the handlebars decides where it displays in the html
