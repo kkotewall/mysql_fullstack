5066991626e558280b5b680bdef6cfac669cae4c
Kayla <k.kotewall@gmail.com>
2018-03-04 14:15:52 -0600

    seeds

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO workout (id, exercise_name, completed)
+VALUES ("pushups", "false");
+
+INSERT INTO workout (id, exercise_name, completed)
+VALUES ("jogging", "false");
+
+INSERT INTO workout (id, exercise_name, completed)
+VALUES ("jumping jacks", "false");
+
