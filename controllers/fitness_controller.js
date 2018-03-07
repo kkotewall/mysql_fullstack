488cdc55b95df5a4279f80aefb79c398d273757b
Kayla <k.kotewall@gmail.com>
2018-03-06 21:34:00 -0600

    controller theme update

--- controllers/fitness_controller.js
+++ controllers/fitness_controller.js
@@ -6,35 +6,35 @@ var router = express.Router();
 var fitnessModel = require("../models/fitness.js");
 
 
-// Create all our routes and set up logic within those routes where required.
+// create all routes
 router.get("/", function(req, res) {
   cat.all(function(data) {
     var hbsObject = {
-      cats: data
+      workout: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
 
-router.post("/api/cats", function(req, res) {
-  cat.create([
-    "name", "sleepy"
+router.post("/api/workout", function(req, res) {
+  workout.create([
+    "name", "skiing"
   ], [
-    req.body.name, req.body.sleepy
+    //req.body.name, req.body.sleepy
   ], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
 
-router.put("/api/cats/:id", function(req, res) {
+router.put("/api/workout/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
-  cat.update({
-    sleepy: req.body.sleepy
+  workout.update({
+    //sleepy: req.body.sleepy
   }, condition, function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
@@ -45,18 +45,6 @@ router.put("/api/cats/:id", function(req, res) {
   });
 });
 
-router.delete("/api/cats/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  cat.delete(condition, function(result) {
-    if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
 
 // Export routes for server.js to use.
 module.exports = router;
